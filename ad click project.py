45a35c58cd8317c3c6e4b8cb4ed118ad57d71ade
brandon728 <brandon.carter728@gmail.com>
2024-12-12 16:48:23 -0800

    EDA begins

---
+++ ad click project.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+data = pd.read_csv("/kaggle/input/advertisement-click-on-ad/advertising.csv")
