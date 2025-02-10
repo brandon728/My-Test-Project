1c737fb1aab012218278e33849ff13242db6d79a
brandon728 <brandon.carter728@gmail.com>
2025-02-10 15:31:44 -0800

    removed test files and continued EDA

--- ad click project.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set_style('whitegrid')
-plt.style.use("fivethirtyeight")
-data = pd.read_csv("/kaggle/input/advertisement-click-on-ad/advertising.csv")
